2df0b48af3ba1800a5c611415b4b429ddbc76c0f
marmo002 <marmo_02@hotmail.com>
2017-10-27 15:34:03 -0400

    add pry gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'sorcery'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry-rails'
   gem 'faker'
 end
 
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -15,7 +15,7 @@ class ProjectTest < ActiveSupport::TestCase
 
   test 'project is invalid without owner' do
     project = new_project
-    project.user = nil
+    project.user_id = nil
     project.save
     assert project.invalid?, 'Project should not save without owner.'
   end
