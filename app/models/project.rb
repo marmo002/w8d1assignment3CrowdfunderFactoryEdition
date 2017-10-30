25b5ee925820979756e348a0aa98b4f8c5ad0be3
soni17 <elb004@gmail.com>
2017-10-30 16:29:04 -0400

    validation for project goal and reward amount. Must be positive number.

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,5 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :end_date, :user_id, presence: true
-
+  validates :goal , numericality: {greater_than: 0}
 end
